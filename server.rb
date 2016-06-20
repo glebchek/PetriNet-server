f407fa3225b339ea86432f39ad0ade261b61ad9c
Gleb Chekushin <mail@glebchek.com>
2016-06-20 23:07:48 +0500

    Remove unnecessary route

--- server.rb
+++ server.rb
@@ -6,10 +6,6 @@ get '/' do
     redirect '/index.html'
 end
 
-get '/:file' do
-  redirect '/#{:file}'
-end
-
 post '/' do
   values = "{\"mark\": [3,1,2], \"transfers\": [[[0,0],[1]],[[1],[1,2]],[[0],[0,2]],[[0,2],[]]]}"
   values = JSON.parse(values)
